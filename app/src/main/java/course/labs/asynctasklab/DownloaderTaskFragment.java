99c3629449d9ef7cddb2b88c0423a868a9865109
mgumiero9 <mgumiero9@gmail.com>
2015-04-05 20:35:39 -0300

    working

--- app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java
+++ app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java
@@ -29,17 +29,19 @@ public class DownloaderTaskFragment extends Fragment {
 		
 		// TODO: Create new DownloaderTask that "downloads" data
 
+        DownloaderTask myDownloaderTask = new DownloaderTask();
         
 		
 		// TODO: Retrieve arguments from DownloaderTaskFragment
 		// Prepare them for use with DownloaderTask. 
 
-        
+        Bundle myBundle = this.getArguments();
+        ArrayList<Integer> resourceIDS = myBundle.getIntegerArrayList("friends");
         
         
 		// TODO: Start the DownloaderTask 
 		
-        
+        myDownloaderTask.execute(resourceIDS);
 
 	}
 
@@ -73,20 +75,46 @@ public class DownloaderTaskFragment extends Fragment {
 	// out). Ultimately, it must also pass newly available data back to 
 	// the hosting Activity using the DownloadFinishedListener interface.
 
-//	public class DownloaderTask extends ... {
-	
+    public class DownloaderTask extends AsyncTask<ArrayList<Integer>, Void, String[]>{
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p/>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param params The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected String[] doInBackground(ArrayList<Integer>... params) {
 
-    
-    
-    
-    
-    
-    
-    
+            ArrayList<Integer> data = params[0];
+            Integer[] dataArray = new Integer[data.size()];
+
+            for(int i=0;i<data.size();i++){
+                dataArray[i] = data.get(i);
+            }
+            return downloadTweets(dataArray);
+        }
+
+        @Override
+        protected void onPostExecute(String[] myResult) {
+
+            mCallback.notifyDataRefreshed(myResult);
+
+        }
+
+
+
+    }
         // TODO: Uncomment this helper method
 		// Simulates downloading Twitter data from the network
 
-        /*
          private String[] downloadTweets(Integer resourceIDS[]) {
 			final int simulatedDelay = 2000;
 			String[] feeds = new String[resourceIDS.length];
@@ -124,7 +152,7 @@ public class DownloaderTaskFragment extends Fragment {
 
 			return feeds;
 		}
-         */
+
 
 
     
